d94615ccac990e4cecf8f884d4937ced2584c74a
Elin Ronda <elro2506@student.miun.se>
2026-01-23 15:10:54 +0100

    Webbplats klar. Innehåller funktionerna som krävs

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,9 @@ function myFunction() {
   var nav = document.getElementById("myLinks");
   nav.classList.toggle("is-open");
 }
+
+//Inspiration från Stackoverflow för att hämta ett random nummer mellan 0 och 100
+function Random() {
+        var rnd = Math.floor(Math.random() * 100);
+        document.getElementById('tb').value = rnd;
+    }
